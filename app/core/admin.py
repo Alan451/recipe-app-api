7912c73868faca3e10cf7821d0655f2f8584f285
Students Web Committee <swc@Studentss-iMac.local>
2021-03-11 23:22:18 +0530

    created custom UserAdmin for the customised User model

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,23 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from . import models
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+@admin.register(models.User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (_('Personal info'), {'fields': ('email', 'name', 'password')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
